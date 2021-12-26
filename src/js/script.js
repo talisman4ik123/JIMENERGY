b9b6752a333eb5ebbcb63e6f517e3ba26ae50e63
Dmitriy <zaitsev.d.e.2000@gmail.com>
2021-12-26 05:52:31 +0300

    add sections how we work, our projects, catalog, our partners, feedback

--- src/js/script.js
+++ src/js/script.js
@@ -12,13 +12,29 @@ burger('header-top__menu');
 // blurElements();
 
 
-// import {isMobile} from "./libs/isMobile.js";
-// if (isMobile.any()) {
-//     document.body.classList.add('_mobile');
-// } else {
-//     document.body.classList.add('_pc');
-// }
+import { isMobile } from "./libs/isMobile.js";
+if (isMobile.any()) {
+    document.body.classList.add('_mobile');
+} else {
+    document.body.classList.add('_pc');
+}
 
+function trasformPlaceholder(input) {
+    const inp = document.querySelector(input);
+    const placeHolder = inp.previousElementSibling;
+    inp.onfocus = function () {
+        placeHolder.classList.add('active');
+    };
+    inp.onblur = function () {
+        if (inp.value == "") {
+            placeHolder.classList.remove('active');
+        }
+    };
+}
+
+trasformPlaceholder("#name");
+trasformPlaceholder("#phone");
+trasformPlaceholder("#message");
 
 //import {pagination} from "./libs/pagination.js";
 //pagination(list, item, itemCount);
